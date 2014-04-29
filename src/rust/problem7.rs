4259b6d24b33174dd1bc726c446ea4635e0e4045
Kevin Most <kevin@kevinmost.com>
2014-04-28 23:14:17 -0400

    cleaned up comments a bit

--- src/rust/problem7.rs
+++ src/rust/problem7.rs
@@ -1,5 +1,5 @@
 // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-// What is the 10 001st prime number?
+// Problem: What is the 1 millionth prime number?
 
 fn main() {
 	// Note: we put ": uint" after every variable because these variables are "unsigned integers"
